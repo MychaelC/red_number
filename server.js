cc2fd05b399aa0570d681e38e4c4c1b1a19958fc
Mychael Contreras <miketw1212@hotmail.com>
2023-01-23 17:57:21 -0700

    added default json and config file

--- server.js
+++ server.js
@@ -2,3 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const config = require('config');
+
+const app = express();
+app.use(express.json());
+
+//used in production to serve client files
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
+
+
+//connecting to mongoDB and then running server on PORT 3000
+const dbURI = config.get('dbURI');
+const port = process.env.PORT || 3000;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(port))
+  .catch((err) => console.log(err));
